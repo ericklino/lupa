f79fd5a3e4af105057e2bde6ba75b303e2f5ca22
ericklino <e7.ifms@gmail.com>
2017-05-25 17:46:21 -0400

    ajuste

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
-
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {sessions: "sessions"}
-
-  #root 'home#index'
 
   # About
   get 'quem-somos' => 'about#index', as: :about
@@ -14,27 +10,17 @@ Rails.application.routes.draw do
   get 'contato' => 'contacts#index', as: :contacts
   post 'contato' => 'contacts#create', as: :create_contact
 
-  #  resources:dispositivos
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'feeder#index'
   get 'dispositivos' => 'devices#index', as: :list_devices
 
 
   get 'home' => 'home#index', as: :home
 
-
-
   #avaliacao
   get 'avaliacao/dispositivo/:id' => 'question#evaluation', as: :evaluations
   post 'avaliacao/dispositivo/:id' => 'question#create_evaluation'
   get 'minhasavaliacoes' => 'evaluations#index', as: :list_evaluations
-
-
 
   # reports
   get 'dispositivos/selecao' => 'reports#index', as: :device_seletion
